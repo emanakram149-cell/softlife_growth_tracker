80f415de18068ceeb6af1cd072247be0391f5903
emanakram149-cell <emanakram149@gmail.com>
2026-05-13 01:02:17 -0700

    Delete api directory

--- api/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/helpers.php';
-setHeaders();
-
-$token = $_SERVER['HTTP_X_TOKEN'] ?? '';
-if ($token && strlen($token) === 64) {
-    getDB()->prepare('DELETE FROM sessions WHERE token = ?')->execute([$token]);
-}
-ok(['message' => 'Logged out.']);
